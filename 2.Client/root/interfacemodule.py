17a013e1ab271093cce8f7610ed2b0d2de8383f6
blackdragonx61 <33325253+blackdragonx61@users.noreply.github.com>
2022-03-13 15:28:36 +0300

    Tutorial Correction(Change -> Add)

--- 2.Client/root/interfacemodule.py
+++ 2.Client/root/interfacemodule.py
@@ -3,7 +3,7 @@
 			if app.ENABLE_DRAGON_SOUL_SYSTEM:
 				self.wndDragonSoul.HighlightSlot(inven_pos)
 
-#Change
+#Add
 		elif player.INVENTORY == inven_type:
 			if app.BL_ENABLE_PICKUP_ITEM_EFFECT:
 				self.wndInventory.HighlightSlot(inven_pos)
